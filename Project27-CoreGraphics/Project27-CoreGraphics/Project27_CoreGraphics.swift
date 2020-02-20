125c85ffd2341836ab35b85b3afb2111fc319b0c
icedoutxcv <icedoutxcv@gmail.com>
2020-02-20 15:16:35 +0100

    project finished, added updateUI function

---
+++ Project27-CoreGraphics/Project27-CoreGraphics/Project27_CoreGraphics.swift
@@ -0,0 +1,11 @@
+//
+//  Project27_CoreGraphics.swift
+//  Project27-CoreGraphics
+//
+//  Created by Kamil Bloch on 26/11/2019.
+//  Copyright © 2019 Kamil Bloch. All rights reserved.
+//
+
+class Project27_CoreGraphics {
+
+}
